50b94f99b4111cde1386ab149f755d508b637810
Huy Ngo <hngo4619@outlook.com>
2018-01-09 16:00:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 data = sys.stdin.readlines()
 with open("output.txt", "w") as text_file:
 	for index, i in enumerate(data):
-		str = "'" + i.strip() + "'"
+		str = "'" + i.strip() + " '"
 		if index < (len(data) - 1):
 			str += " +\n"
 		else:
