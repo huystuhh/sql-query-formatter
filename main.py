4563ce476daa4138159641a4f922b292d06ce42e
Huy Ngo <hdngo@us.ibm.com>
2018-01-09 10:54:53 -0600

    Adding files

---
+++ main.py
@@ -0,0 +1,13 @@
+# Quick Python function to read lines from a file and format it
+# Used to make my life easier when needing to format a bunch of SQL queries for JavaScript 
+# To run: cat <input_file> | python main.py
+import sys
+data = sys.stdin.readlines()
+with open("output.txt", "w") as text_file:
+	for index, i in enumerate(data):
+		str = "'" + i.rstrip('\n') + "'"
+		if index < (len(data) - 1):
+			str += " +\n"
+		else:
+			str += ";"
+		text_file.write(str)
